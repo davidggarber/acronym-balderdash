0a104dbd63b0350a1eb0e1a7bf11733189c78a4d
David Garber <dgarber@microsoft.com>
2026-07-14 11:41:08 -0700

    Fix link to list.mdb

---
+++ rounds/score.js
@@ -0,0 +1,33 @@
+addRound(document.currentScript, {
+    acronym: "SCORE",
+    guesses: [
+        {
+            definition: "Service Corps Of Retired Executives",
+            author: "",
+            flag: Correct,
+            notes: "A business mentoring non-profit",
+        },
+        {
+            definition: "",
+            author: "",
+            flag: CoinFlip,
+        },
+        {
+            definition: "",
+            author: "",
+            flag: MontyHall,
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        }
+    ]
+});
---
+++ rounds/solve.js
@@ -0,0 +1,33 @@
+addRound(document.currentScript, {
+    acronym: "SOLVE",
+    guesses: [
+        {
+            definition: "Strategies of Limiting Violent Episodes",
+            author: "",
+            flag: Correct,
+            notes: "Crisis management group in Mansfield, Maryland",
+        },
+        {
+            definition: "",
+            author: "",
+            flag: CoinFlip,
+        },
+        {
+            definition: "",
+            author: "",
+            flag: MontyHall,
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        }
+    ]
+});
