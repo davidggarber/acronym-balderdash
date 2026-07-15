e34ee49863d85d9129bcb4798a33d8c5e4145dde
davidggarber <davidggarber@hotmail.com>
2026-07-15 04:49:33 -0700

    Last 2 placeholders

---
+++ rounds/alpaca.js
@@ -0,0 +1,31 @@
+addRound(document.currentScript, {
+    acronym: "",
+    guesses: [
+        {
+            definition: "A Language for Programming Arbitrary Cellular Automata",
+            author: "",
+            correct: true,
+            notes: "A Metalanguage for Pathological Programming",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        }
+    ]
+});
---
+++ rounds/cobra.js
@@ -0,0 +1,31 @@
+addRound(document.currentScript, {
+    acronym: "",
+    guesses: [
+        {
+            definition: "Consolidated Omnibus Budget Reconciliation Act",
+            author: "",
+            correct: true,
+            notes: "The 1985 law that allows ex-employees to continue health insurance",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        },
+        {
+            definition: "",
+            author: "",
+        }
+    ]
+});
