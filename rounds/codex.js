6639c112ed89acf5f3fb4c8613615d95686e8059
Halle Remash <halleremash@microsoft.com>
2026-07-18 12:34:34 -0700

    add codex

--- rounds/codex.js
+++ rounds/codex.js
@@ -8,16 +8,16 @@ addRound(document.currentScript, {
             notes: "A step in Nuclear Magnetic Resonance (NMR) spectroscopy"
         },
         {
-            definition: "",
-            author: "",
+            definition: "Committee on Decrypting Extraterrestrial X-Rays",
+            author: "Game On! (Team 16)",
         },
         {
-            definition: "",
-            author: "",
+            definition: "Container of Dangerous EXplosives",
+            author: "SS Estonia (Team 43)",
         },
         {
-            definition: "",
-            author: "",
+            definition: "Council of Directly Elected Executives",
+            author: "🫖heads (Team 56)",
         },
         {
             definition: "",
