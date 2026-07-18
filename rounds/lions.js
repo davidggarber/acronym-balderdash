3d2262ec30037968e7c0c473a477d43833873cf3
Halle Remash <halleremash@microsoft.com>
2026-07-18 13:06:22 -0700

    add lions

--- rounds/lions.js
+++ rounds/lions.js
@@ -5,26 +5,27 @@ addRound(document.currentScript, {
             definition: "Local Interpretation of National Standards",
             author: "",
             correct: true,
+            notes: "The process of modifying national or international standards to fit local needs and conditions",
         },
         {
-            definition: "",
+            definition: "Lady Into Only Nocturnal Studs",
+            author: "Leslie (Team 16)",
+        },
+        {
+            definition: "League of Intelligence Officers, Northern Subcommittee",
+            author: "We Put The Brick On The Accelerator​ (Team 53)",
+        },
+        {
+            definition: "Least Individual Ordinal Number System",
+            author: "Cat Ladies (Team 5)",
+        },
+        {
+            definition: "Low Input/Output Noise Signature",
             author: "",
         },
         {
-            definition: "",
-            author: "",
-        },
-        {
-            definition: "",
-            author: "",
-        },
-        {
-            definition: "",
-            author: "",
-        },
-        {
-            definition: "",
-            author: "",
+            definition: "Logistics Integration Over Neural Systems",
+            author: "Matt (Team 48)",
         }
     ]
 });
